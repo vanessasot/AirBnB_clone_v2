cea518785e66e6b13e00376df9d1cc8963fce572
jpenariasholberton <1875@holbertonschool.com>
2020-12-15 18:06:53 -0500

    fix all privileges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,7 +6,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- The password for hbnb_dev must be set to hbnb_dev_pwd
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- hbnb_dev must have all privileges on the hbnb_dev_db database
-GRANT SELECT ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- hbnb_dev must have SELECT privilege on the performance_schema database.
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;

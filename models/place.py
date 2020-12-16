41f2f937f343c3c50e4c6bd0610f18420a8eba07
Vanessa <vannexxaa@gmail.com>
2020-12-16 20:39:03 +0000

    Update place.py file

--- models/place.py
+++ models/place.py
@@ -2,6 +2,7 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):

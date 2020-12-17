d74ecb5dd6eb8099ca190adbdb4480dc9d5eb172
Vanessa <vannexxaa@gmail.com>
2020-12-17 19:33:04 +0000

    Updated file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,3 +65,7 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
             self.save()
+
+    def close(self):
+        """Deserializing the JSON file"""
+        reload(self)

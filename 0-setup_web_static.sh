daf855d941ec845d3d234ee1778b227d7e19cbc8
Vanessa <vannexxaa@gmail.com>
2020-12-23 03:01:07 +0000

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,5 +14,5 @@ echo "<html>
       </html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/hbnb_static/; }'  /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/; }'  /etc/nginx/sites-available/default
 sudo service nginx restart

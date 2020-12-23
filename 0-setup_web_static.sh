2bcdbd1dae24eb39000c86bed1bf1144c833eb85
Vanessa <vannexxaa@gmail.com>
2020-12-23 01:50:15 +0000

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,15 +3,16 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
-sudo chmod 666 /var/www/html/index.nginx-debian.html
-mkdir -p data/{web_static/{releases/test/,shared}}
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "<html>
-          <head></head>
-          <body>Hello, Holberton</body>
+        <head>
+        </head>
+        <body>
+          Holberton School
+        </body>
       </html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown ubuntu:ubuntu /data/
-sudo sed -i 's/location / {/a location /hbnb_static { alias /data/web_static/current/hbnb_static/;/'  /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/hbnb_static/; }'  /etc/nginx/sites-available/default
 sudo service nginx restart
-
-

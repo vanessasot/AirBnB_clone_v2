d5e2a71ac6b084019ad2e585a1283b80d0bd50bd
Vanessa <vannexxaa@gmail.com>
2021-01-19 23:30:27 +0000

    Starts a Flask web application with a four routes

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Starts a Flask web application with four routes"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", defaults={'text': 'is cool'})
+@app.route("/python/", defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    return "Python {}".format(text.replace("_", " "))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
